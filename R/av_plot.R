16e41884f295f8131ba97c1300269dd86c6b8900
Tom August <tomaug@ceh.ac.uk>
2022-07-16 22:26:14 +0100

    improve spectrogram plotting

---
+++ R/av_plot.R
@@ -0,0 +1,39 @@
+av_plot <- function (x, y, dark = TRUE, legend = '', keep.par = FALSE, 
+          useRaster = TRUE, vline = FALSE, hline = NULL, ...) 
+{
+  if (!isTRUE(keep.par)) {
+    oldpar <- par(no.readonly = TRUE)
+    on.exit(par(oldpar))
+  }
+  col <- if (isTRUE(dark)) {
+    par(bg = "black", col.axis = "white", fg = "white", 
+        family = "mono", font = 2, col.lab = "white", 
+        col.main = "white")
+    c("#FCFFA4FF", "#F5DB4BFF", "#FCAD12FF", 
+      "#F78311FF", "#E65D2FFF", "#CB4149FF", 
+      "#A92E5EFF", "#85216BFF", "#60136EFF", 
+      "#3A0963FF", "#140B35FF", "#000004FF")
+  }
+  else {
+    c("#FFFFC8", "#FFF4B7", "#FBE49A", 
+      "#F8D074", "#F7BA3C", "#F5A100", 
+      "#F28400", "#ED6200", "#E13C00", 
+      "#C32200", "#A20706", "#7D0025")
+  }
+  par(mar = c(5, 5, 3, 3), mex = 0.6)
+  image(attr(x, "time"), attr(x, "frequency")/1000, 
+        t(x), xlab = "TIME", ylab = "FREQUENCY (kHZ)", 
+        col = col, useRaster = useRaster, ...)
+  if (legend!='') {
+    legend("topright", legend = legend, pch = "", 
+           xjust = 1, yjust = 1, bty = "o", cex = 0.7)
+  }
+  if (length(vline)) {
+    abline(v = vline, lwd = 2)
+  }
+  if (!is.null(hline)){
+    for(i in hline){
+      abline(h = i, col = rgb(1,1,1,0.3, maxColorValue = 1))
+    }
+  }
+}
--- R/write_spectro.R
+++ R/write_spectro.R
@@ -5,10 +5,12 @@ function(file, TD = NULL, samp_freq = 256000, tempDir = tempdir(), verbose = TRU
   if(verbose) cat('Creating spectrogram')
   tf1 <- tempfile(fileext = '.png', tmpdir = tempDir)
   suppressMessages({
-    png(filename = tf1, width = 1024, height = 1024*0.75)
+    png(filename = tf1, width = 8, height = 8*0.75, units = 'in', res = 300)
       fft_data <- av::read_audio_fft(file, window = av::hanning(2048), overlap = 0.2)
-      plot(fft_data,
-           main = 'Spectrogram of full sequence')
+      av_plot(fft_data, 
+              hline = seq(10, 120, 10),
+              yaxp = c(0,120,12),
+              legend = 'Full sequence')
     dev.off()
     fnames <- tf1
   })
@@ -27,17 +29,17 @@ function(file, TD = NULL, samp_freq = 256000, tempDir = tempdir(), verbose = TRU
       
       tf2 <- tempfile(fileext = '.png', tmpdir = tempDir)
       suppressMessages({
-        png(filename = tf2, width = 1024, height = 1024*0.75)
+        png(filename = tf2, width = 8, height = 8*0.75, units = 'in', res = 300)
         fft_data <- av::read_audio_fft(file,
                                        window = av::hanning(512),
                                        overlap = 0.5, 
                                        start_time = bins[1], end_time = bins[1]+bin_window)
-        plot(fft_data,
-             main = 'Part sequence')
+        av_plot(fft_data,
+                hline = seq(10, 120, 10),
+                yaxp = c(0,120,12), 
+                legend = 'Part sequence')
         dev.off()
       })
-      plot(fft_data,
-           main = 'Part sequence')
       fnames <- c(tf2, fnames)  
     }
   }
